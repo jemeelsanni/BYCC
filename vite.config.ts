c69831b85b5b839fda5988312facd1e3f3486b8a
Jemeel <jemeelsanni@gmail.com>
2025-05-01 16:50:23 +0100

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck - Ignoring TypeScript errors to allow build to proceed
+
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -14,5 +12,9 @@ export default defineConfig({
       '@utils': path.resolve(__dirname, './src/utils')
     }
   },
-  base: '/'
+  base: '/',
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment'
+  }
 });
